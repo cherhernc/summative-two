f490df338f6aad1acba200fa8acac92c24ae4828
cherhernc <62526192+cherhernc@users.noreply.github.com>
2020-04-03 13:03:23 +1300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("HELLO WORLD!");
+
+var navButton = document.getElementById('navButton');
+var menu = document.getElementById("menu");
+
+navButton.addEventListener('click' , toggleMenu);
+
+function toggleMenu() {
+	menu.classList.toggle("displayMenu");
+	navButton.classList.toggle("fa-times");
+
+}
